b0e48a84b12de806ecee5a824bea24c0d93eb157
Nomi-1 <64132192+Nomi-1@users.noreply.github.com>
2021-01-11 12:11:38 +0100

    add 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Head from '../components/head'
+import errorStyle from "../styles/404.module.scss"
+
 
 const NotFound = () =>{
     return(
         <Layout>
             <Head title="404"/>
-            <h1>PAge not found</h1>
-            <p><Link to="/">Back to Page</Link></p>
+            <div className={errorStyle.error}>
+            <h1>404</h1>
+            <h2>Sorry, you are wrong</h2>
+            <p><Link to="/">Back to the right path</Link></p>
+            </div>
         </Layout>
 
     )
